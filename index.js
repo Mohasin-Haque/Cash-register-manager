bd012e7cdb1b3a08874342c09f2dc65b918f635f
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-08-29 23:20:02 +0530

    added files

---
+++ index.js
@@ -0,0 +1,77 @@
+const billAmount = document.querySelector("#bill-amount");
+const nextButton = document.querySelector("#next-button");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const cashCheck = document.querySelector("#cash-check");
+const tableDisplay = document.querySelector(".change-table");
+
+
+
+cashCheck.style.display = "none";
+tableDisplay.style.display = "none";
+
+// var numberOfNotes;
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+nextButton.addEventListener("click", billAmnt);
+hideMessage();
+checkButton.addEventListener("click", changeCalculater);
+// hideMessage();
+
+function billAmnt() {
+    if (billAmount.value > 0) {
+        cashCheck.style.display = "flex";
+    } else {
+        showMessage("Paisa do..😐");
+    }
+}
+
+
+function changeCalculater() {
+
+    if (cashGiven.value >= billAmount.value) {
+        tableDisplay.style.display = "block";
+        hideMessage();
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        calculateChange(amountToBeReturned);
+        if (amountToBeReturned == 0) {
+            showMessage("Paisa ho gya🙄 ...  Ab kya chahiye..🤨 ");
+            tableDisplay.style.display = "none";
+        }
+    } else {
+        tableDisplay.style.display = "none";
+        showMessage("Pura paisa do nhi to bartan manjo.😏");
+    }
+}
+
+// function returnChange(amountToBeReturned){
+//     for(let i = 0; i< availableNotes>length; i++){
+//         const noOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+//     amountToBeReturned %= availableNotes[i];
+//     typeNoOfNotes[i].innerText = noOfNotes;
+//     }
+// }
+
+
+function calculateChange(amountToBeReturned) {
+    // go over all the availables
+    for (let i = 0; i < availableNotes.length; i++) {
+        //no. of notes need for denomination
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        //amount left after the number of notes needed
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
